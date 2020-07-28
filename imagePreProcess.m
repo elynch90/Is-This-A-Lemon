0fbaa1e89c8364aa2a3569bc9c1064a1ec2488fb
Elliott Lynch Mac 2016 <StratosphericSciences@Elliotts-MacBook-Pro-3.local>
2020-07-28 00:50:33 -0700

    Image PreProcess Update
    Added folders for positive and negative training set as well as integrated automatic labling

--- imagePreProcess.m
+++ imagePreProcess.m
@@ -3,42 +3,62 @@
 % TODO: optimization
 
 pkg load image % import forge imag pkg
-% load image matrix and label vector
-imgFolder = dir('rawTrainingImgs');
+%========== load image matrix and label vector ==========
+trainingFolderPos = dir('rawTrainingImgs/postive');
+trainingFolderNeg = dir('rawTrainingImgs/negative');
 validationFolderNeg = dir('validationImgs/negative');
 validationFolderPos = dir('validationImgs/positive');
 
-m = length(imgFolder) - 3; % number of images in the dir to process excluding meta data
-mValidationNeg = length(validationFolderNeg)
-mValidationPos = length(validationFolderPos)
-X = zeros(m, 400); % initialize output matrix 400 columns from 20 x 20 images
+mPos = length(trainingFolderPos) - 3; % number of images in the positive training set excluding meta data
+mNeg = length(trainingFolderNeg) - 3; % number of images in the negative training set excluding metadata
+mValidationNeg = length(validationFolderNeg);
+mValidationPos = length(validationFolderPos);
+X = zeros(mPos + mNeg, 400); % initialize output matrix 400 columns from 20 x 20 images
 valTestSet = zeros(mValidationNeg + mValidationPos, 400);
 
 %========= preprocess training set images =========
+% prepare negative training examples
+printf('Preparing negative training examples...\n');
+
 printf('Running...\n');
-for i=4:m, % for each image excluding metadata
-    % display(imgFolder(i).name)
-    curImg = imread(strcat('rawTrainingImgs/', imgFolder(i).name)); % current img itteration
+for i=4:mNeg, % for each image excluding metadata
+    display(trainingFolderNeg(i).name)
+    curImg = imread(strcat('rawTrainingImgs/negative/', trainingFolderNeg(i).name)); % current img itteration
     % convert  to greyscale
     greyscaleImg = rgb2gray(curImg);
     processedImg = imresize(greyscaleImg, [20, 20]);  % resize image to 20 x 20
    
-
     % reshape current image as vector
     processedImg = reshape(processedImg, [1, 400]);
     % store in output matrix
     X(i - 3, :) = processedImg;
 end
 
-% add y vector with labels
-% because this is a binary classifier we must label these as the positive class "1"
-y = ones(m, 1);
+% prepare positive training examples
+printf('Preparing positive training examples...\n');
+
+printf('Running...\n');
+for i=4:mPos, % for each image excluding metadata
+    display(trainingFolderPos(i).name)
+    curImg = imread(strcat('rawTrainingImgs/positive/', trainingFolderPos(i).name)); % current img itteration
+    % convert  to greyscale
+    greyscaleImg = rgb2gray(curImg);
+    processedImg = imresize(greyscaleImg, [20, 20]);  % resize image to 20 x 20
+   
+    % reshape current image as vector
+    processedImg = reshape(processedImg, [1, 400]);
+    % store in output matrix
+    X(mNeg + i - 3, :) = processedImg;
+end
+
+%========== create the labels for the validation set ==========
+y = [zeros(mNeg,1); ones(mValidationPos,1)] % add y vector with labels
 
 %========= Preprocessing for validation testing set =========
 % prepare negative testing examples
 printf('Preparing negative testing examples...\n');
 for i=4:mValidationNeg, % for each image excluding metadata
-    display(validationFolderNeg(i).name)
+    validationFolderNeg(i).name
     curImgNeg = imread(strcat('validationImgs/negative/', validationFolderNeg(i).name)); % current img itteration
     % convert  to greyscale
     greyscaleImg = rgb2gray(curImgNeg);
@@ -48,7 +68,7 @@ for i=4:mValidationNeg, % for each image excluding metadata
     processedImg = reshape(processedImg, [1, 400])
     processedImg
     % store in output matrix
-    valTestSet(i -3, :) = processedImg;
+    valTestSet(i - 3, :) = processedImg;
 end
 
 % prepare positve testing examples
@@ -61,7 +81,7 @@ for i=4:mValidationPos, % for each image excluding metadata
     processedImg = imresize(greyscaleImg, [20, 20]);  % resize image to 20 x 20
 
     % reshape current image as vector
-    processedImg = reshape(processedImg, [1, 400])
+    processedImg = reshape(processedImg, [1, 400]);
     processedImg
     % store in output matrix
     valTestSet(mValidationNeg + i -3, :) = processedImg;
